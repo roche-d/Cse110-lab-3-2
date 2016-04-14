7d1b08f1842b9ed9db9eed4c619a9a1e7b860aec
roche_d <roche_d@epitech.eu>
2016-04-14 15:03:11 -0700

    Initial commit

---
+++ app/src/main/java/com/example/roche_d/cse110_lab_3_2/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.roche_d.cse110_lab_3_2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText eta = (EditText) findViewById(R.id.editText);
+                EditText etb = (EditText) findViewById(R.id.editText2);
+                TextView tv = (TextView) findViewById(R.id.textView);
+
+                try {
+                    int a = Integer.parseInt(eta.getText().toString());
+                    int b = Integer.parseInt(etb.getText().toString());
+                    tv.setText(Integer.toString(sum(a, b)));
+                } catch (Exception e) {
+                    tv.setText("Invalid input");
+                }
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public int sum(int a, int b)
+    {
+        return a + b;
+    }
+}
